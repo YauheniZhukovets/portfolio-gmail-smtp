fc2a0b77b3c9184416d2b73f5c9bcb641fbe4f4e
YauheniZhukovets <stewart-8@mail.ru>
2023-03-04 23:35:24 +0400

    added dotenv

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const app = express()
-app.use(cors({
-    credentials: true,
-    origin: true
-}))
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
