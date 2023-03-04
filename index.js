a9f4cdba21b7294023db89ae71bfe3994756aa5f
YauheniZhukovets <stewart-8@mail.ru>
2023-03-04 21:50:34 +0400

    added vercel config

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: true
+}))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
